c0b2bba307545f4036851e03fb271fa2a9909d8f
Warren Wrate <wawrate@gmail.com>
2025-02-28 13:42:15 -0800

    changes to app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 def add(a, b):
     return a + b
 
+def sub(a, b):
+    return a - b
+
 if __name__ == "__main__":
     print("Hello, GitHub Actions!")
--- test_app.py
+++ test_app.py
@@ -2,4 +2,4 @@ from app import add
 
 def test_add():
     assert add(2, 3) == 5
-    assert add(-1, 1) == 1
+    assert add(-1, 1) == 0
